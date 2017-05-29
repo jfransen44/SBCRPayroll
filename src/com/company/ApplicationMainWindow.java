5c15f5d7930749f798e00caceee5ed7b264a9fec
Jeremy Fransen <jeremyfransen@Jeremys-MacBook-Pro.local>
2017-05-29 13:24:42 -0700

    close dialog now active, cleaning up

--- src/com/company/ApplicationMainWindow.java
+++ src/com/company/ApplicationMainWindow.java
@@ -27,22 +27,26 @@ public class ApplicationMainWindow extends VBox {
     private static VBox container;
     private static HBox topHBox, middleHBox, bottomHBox;
     private static Button selectReportsButton, processButton, removeFilesButton, saveButton;
-    private static Label toOpenLabel, statusLabel;
+    private static Label toOpenLabel, statusLabel, startDateLabel, endDateLabel;
     private static ListView<File> fileListView;
     private static DatePicker startDatePicker, endDatePicker;
     private static LocalDate startDate, endDate;
     private static PayrollCalculator payrollCalculator;
+    private static Stage window;
+    private static boolean isFileSaved = true;
 
 
     public static void display(){
 
-        Stage window = new Stage();
+        window = new Stage();
         container = new VBox();
         topHBox = new HBox();
         middleHBox = new HBox();
         bottomHBox = new HBox();
         toOpenLabel = new Label("Files to open");
         statusLabel = new Label();
+        startDateLabel = new Label("Start date:");
+        endDateLabel = new Label("End date:");
         fileListView = new ListView<>();
         fileListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         startDatePicker = new DatePicker();
@@ -52,7 +56,7 @@ public class ApplicationMainWindow extends VBox {
         removeFilesButton = new Button("Remove Selected");
         saveButton = new Button("Save");
         container.getChildren().addAll(topHBox, toOpenLabel, fileListView, statusLabel, middleHBox, bottomHBox);
-        topHBox.getChildren().addAll(startDatePicker, endDatePicker, selectReportsButton);
+        topHBox.getChildren().addAll(startDateLabel, startDatePicker,  endDateLabel, endDatePicker, selectReportsButton);
         middleHBox.getChildren().add(removeFilesButton);
         bottomHBox.getChildren().addAll(processButton, saveButton);
         setLayout();
@@ -65,15 +69,15 @@ public class ApplicationMainWindow extends VBox {
     private static void setLayout(){
         container.setPrefSize(700, 500);
         container.setPadding(new Insets(25));
-        topHBox.setAlignment(Pos.TOP_CENTER);
+        topHBox.setAlignment(Pos.CENTER);
         topHBox.setPadding(new Insets(5));
         topHBox.setMargin(startDatePicker, new Insets(5));
         topHBox.setMargin(endDatePicker, new Insets(5));
         topHBox.setMargin(selectReportsButton, new Insets(5));
-        topHBox.setSpacing(25);
+        topHBox.setSpacing(5);
 
-        /*startDatePicker.setPromptText("Start Date");
-        endDatePicker.setPromptText("End Date");*/
+        startDatePicker.setMaxWidth(120);
+        endDatePicker.setMaxWidth(120);
         final LocalDate START_DATE_REFERENCE = LocalDate.of(2017, 5, 8);
         startDate = LocalDate.now();
 
@@ -134,21 +138,7 @@ public class ApplicationMainWindow extends VBox {
             fileListView.getSelectionModel().clearSelection();
         });
 
-        saveButton.setOnAction(e -> {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-            if (payrollCalculator != null) {
-                FileChooser fileChooser = new FileChooser();
-                fileChooser.setTitle("Save Report");
-                fileChooser.setInitialFileName(startDate.format(formatter) + " - " + endDate.format(formatter) + ".xls");
-                File file = fileChooser.showSaveDialog(container.getScene().getWindow());
-                if (file != null) {
-                    payrollCalculator.saveFile(file.toString());
-                }
-            }
-            else{
-                AlertBox.display("Error", "You have not processed your reports");
-            }
-        });
+        saveButton.setOnAction(e -> saveFile());
 
         processButton.setOnAction(e -> {
             String [] files = new String [6];
@@ -168,12 +158,10 @@ public class ApplicationMainWindow extends VBox {
 
             if (startDate != null){
                 week1Dates = startDate.toString().replaceAll("-" , "_") + "-" + startDate.plusDays(6).toString().replaceAll("-" , "_");
-                //System.out.println(week1Dates);
             }
 
             if (endDate != null){
                 week2Dates = endDate.minusDays(6).toString().replaceAll("-" , "_") + "-" + endDate.toString().replaceAll("-" , "_");
-                //System.out.println(week2Dates);
             }
 
             if (fileList != null && week1Dates != null && week2Dates != null){
@@ -181,6 +169,7 @@ public class ApplicationMainWindow extends VBox {
                     payrollCalculator = new PayrollCalculator(week1Dates, week2Dates);
                     payrollCalculator.processPayroll(files);
                     statusLabel.setText("Payroll processed");
+                    isFileSaved = false;
                 }
                 else{
                     AlertBox.display("Error", "Selected dates and report dates do not match");
@@ -188,5 +177,43 @@ public class ApplicationMainWindow extends VBox {
             }
 
         });
+
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            if (! isFileSaved()){
+                String response = CloseDialog.display();
+
+                if (response == "Yes"){
+                    window.close();
+                }
+                else if (response == "Save"){
+                    saveFile();
+                }
+            }
+            else{
+                window.close();
+            }
+        });
     }
+
+    private static boolean isFileSaved(){
+        return isFileSaved;
+    }
+
+    private static void saveFile(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        if (payrollCalculator != null) {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save Report");
+            fileChooser.setInitialFileName(startDate.format(formatter) + " - " + endDate.format(formatter) + ".xls");
+            File file = fileChooser.showSaveDialog(container.getScene().getWindow());
+            if (file != null) {
+                payrollCalculator.saveFile(file.toString());
+            }
+        }
+        else{
+            AlertBox.display("Error", "You have not processed your reports");
+        }
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,59 +11,13 @@ import static javafx.application.Application.launch;
 
 public class Main extends Application{
 
-    private String week1Dates = "";
-    private String week2Dates = "";
-
-    static String[] files = {"/Users/jeremyfransen/Downloads/PayrollExport_2017_05_08-2017_05_14.csv",
-                             "/Users/jeremyfransen/Downloads/PayrollExport_2017_05_08-2017_05_14-2.csv",
-                             "/Users/jeremyfransen/Downloads/PayrollExport_2017_05_08-2017_05_14-3.csv",
-                             "/Users/jeremyfransen/Downloads/PayrollExport_2017_05_15-2017_05_21.csv",
-                             "/Users/jeremyfransen/Downloads/PayrollExport_2017_05_15-2017_05_21-2.csv",
-                             "/Users/jeremyfransen/Downloads/PayrollExport_2017_05_15-2017_05_21-3.csv"};
-
-
-    static String[] files2 = {"/Users/jeremyfransen/desktop/test files/PayrollExport_2017_04_24-2017_04_30-3.csv",
-                              "/Users/jeremyfransen/desktop/test files/PayrollExport_2017_04_24-2017_04_30.csv",
-                              "/Users/jeremyfransen/desktop/test files/PayrollExport_2017_04_24-2017_04_30-2.csv",
-                              "/Users/jeremyfransen/desktop/test files/PayrollExport_2017_05_01-2017_05_07-2.csv",
-                              "/Users/jeremyfransen/desktop/test files/PayrollExport_2017_05_01-2017_05_07.csv",
-                              "/Users/jeremyfransen/desktop/test files/PayrollExport_2017_05_01-2017_05_07-3.csv"};
-    static String file3 = "/Users/jeremyfransen/desktop/test files/PayrollExport_2017_05_01-2017_05_07-3.csv";
-
-    static String date1 = "2017_05_08-2017_05_14";
-    static String date2 = "2017_05_15-2017_05_21";
     public static void main(String[] args) {
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter file path for report: Week 1 DLV");
-        files[0] = scanner.next();
-        System.out.println("Enter file path for report: Week 2 DLV");
-        files[1] = scanner.next();
-        System.out.println("Enter file path for report: Week 1 Fairview");
-        files[2] = scanner.next();
-        System.out.println("Enter file path for report: Week 2 Fairview");
-        files[3] = scanner.next();
-        System.out.println("Enter file path for report: Week 1 Ventura");
-        files[4] = scanner.next();
-        System.out.println("Enter file path for report: Week 2 Ventura");
-        files[5] = scanner.next();
-        if (PayrollCalculator.checkReports(files, date1, date2)) {
-            PayrollCalculator payrollCalculator = new PayrollCalculator(date1, date2);
-            payrollCalculator.processReports(files);
-            payrollCalculator.processPayroll();
-            payrollCalculator.saveFile("/Users/jeremyfransen/documents/SBCR/PAYROLL/PAYROLL HISTORY/2017/5-8-17 - 5-21-17");
-        } else
-            System.out.println("FILE ERROR");*/
+
         launch(args);
     }
 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        /*Parent root = new ApplicationMainWindow();
-        Scene scene = new Scene(root);
-        primaryStage.setTitle("SBCR PAYROLL");
-        primaryStage.setScene(scene);
-        primaryStage.show();*/
         ApplicationMainWindow.display();
     }
 }
